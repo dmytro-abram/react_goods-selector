6254bfabfd6cf4477e1205aea436c46f596c1a7a
Dmytro Abram <dmytro.study@gmail.com>
2021-05-25 12:03:35 +0300

    Updates

---
+++ precache-manifest.edb1097f4b3898831386ac21cd693e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe5c08ffc1e7447d4a3c4c004c7a536",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb4972d6028bc184c746",
+    "url": "./static/css/main.8065eaa3.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb4972d6028bc184c746",
+    "url": "./static/js/main.fbc4a401.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
